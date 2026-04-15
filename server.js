af5d1584ae56fa49aa26fc575d51f79b597ac0ff
231801136RAMYADEVIK <231801136@rajalakshmi.edu.in>
2026-04-15 10:35:19 +0530

    fixed port for railway

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const API_KEY = process.env.GROQ_API_KEY;
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 console.log("🔑 API KEY:", API_KEY ? "Loaded ✅" : "Missing ❌");
 
